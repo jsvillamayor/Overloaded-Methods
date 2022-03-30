30cb42ee3e82e947071c526089435c684c18f00a
jsvillamayor <78101377+jsvillamayor@users.noreply.github.com>
2022-03-29 17:05:04 -0700

    Add files via upload

---
+++ Overloaded Methods/src/Main.java
@@ -0,0 +1,42 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		double x = add(1.3,2.0,3.0,4.0);
+		System.out.println(x);
+
+	}
+	
+	static int add(int a, int b) {
+		
+		return a+b;
+		
+	}
+	static int add(int a, int b, int c) {
+		
+		return a+b+c;
+		
+	}
+	static int add(int a, int b, int c, int d) {
+		
+		return a+b+c+d;
+		
+	}
+	static double add(double a, double b) {
+		
+		return a+b;
+		
+	}
+	static double add(double a, double b, double c) {
+		
+		return a+b+c;
+		
+	}
+	static double add(double a, double b, double c, double d) {
+		
+		return a+b+c+d;
+		
+	}
+
+}
